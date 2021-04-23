ad260b6022bf606f2fba70c8fa456eb8c13a5451
Eduardo Fernandes <nomeaocalhas3@gmail.com>
2021-04-23 20:55:24 +0100

    Greetings Cog: Documented

--- confraBot/greetings.py
+++ confraBot/greetings.py
@@ -3,6 +3,12 @@ from discord.ext import commands
 
 
 class Greetings(commands.Cog):
+    """
+    An Example Cog that sever as a greter
+
+    adapted from: https://discordpy.readthedocs.io/en/stable/ext/commands/cogs.html#quick-example
+    """
+
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
@@ -10,7 +16,17 @@ class Greetings(commands.Cog):
 
     @commands.command()
     async def hello(self, ctx, *, member: discord.Member = None):
-        """Says hello"""
+        """
+        A command that responds hello to a given user that invoked the command
+
+        Example in a discord chat:
+
+        Alice> $hello
+        Bot> $Hello @Alice
+        Alice> $hello
+        Bot> $Hello @Alice... This feels familiar.
+
+        """
         member = member or ctx.author
         if self._last_member is None or self._last_member.id != member.id:
             await ctx.send(f'Hello {member.name}')
